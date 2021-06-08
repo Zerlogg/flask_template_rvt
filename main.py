81177f05d2a449b849902d9b4b28ea918d752c5e
Emīls Strēlis <>
2021-06-08 12:30:53 +0000

    index

--- main.py
+++ main.py
@@ -16,3 +16,59 @@ def admin_tekst():
 
 
 app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def administatora_lapa():
+    return render_template("administatora_lapa.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def meklet_arstu():
+    return render_template("meklet_arstu.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def meklet_viziti():
+    return render_template("meklet_viziti.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def pieteikt_viziti():
+    return render_template("pieteikt_viziti.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def pievienot_arstu():
+    return render_template("pievienot_arstu.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def pievienot_slimnicu():
+    return render_template("pievienot_slimnicu.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def statistika():
+    return render_template("statistika.html")
+
+
+app.run(host='0.0.0.0', port=8080)
+
+@app.route('/')
+def tekst():
+    return render_template("tekst.html")
+
+
+app.run(host='0.0.0.0', port=8080)
