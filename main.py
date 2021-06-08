6ff42689df83de78e8d8b0fc4ea0d34d99a3d748
DP1IAleskevics <>
2021-06-08 12:54:17 +0000

    index.html

--- main.py
+++ main.py
@@ -7,66 +7,39 @@ app = Flask('app')
 def index():
     return render_template("index.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/admin_tekst')
 def admin_tekst():
     return render_template("admin_tekst.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/administratora_lapa')
 def administatora_lapa():
-    return render_template("administatora_lapa.html")
+    return render_template("administratora_lapa.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/meklet_arstu')
 def meklet_arstu():
     return render_template("meklet_arstu.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/meklet_viziti')
 def meklet_viziti():
     return render_template("meklet_viziti.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/pieteikt_viziti')
 def pieteikt_viziti():
     return render_template("pieteikt_viziti.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/pievienot_arstu')
 def pievienot_arstu():
     return render_template("pievienot_arstu.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/pievienot_slimnicu')
 def pievienot_slimnicu():
     return render_template("pievienot_slimnicu.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/statistika')
 def statistika():
     return render_template("statistika.html")
 
-
-app.run(host='0.0.0.0', port=8080)
-
-@app.route('/')
+@app.route('/tekst')
 def tekst():
     return render_template("tekst.html")
 
